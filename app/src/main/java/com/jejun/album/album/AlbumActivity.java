260d99cfffdedc87f712d521b6b9b685ca4b0561
jejun <jejun.jade@gmail.com>
2019-02-18 21:19:28 +0900

    * delete drawer, add header menu

--- app/src/main/java/com/jejun/album/album/AlbumActivity.java
+++ app/src/main/java/com/jejun/album/album/AlbumActivity.java
@@ -1,12 +1,16 @@
 package com.jejun.album.album;
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.jejun.album.R;
-import com.jejun.album.navigation.NavigationView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -22,11 +26,8 @@ public class AlbumActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_album);
 
-        NavigationView navigationView = findViewById(R.id.navigation_view);
-        navigationView.init();
-
-//        RecyclerView albumList = findViewById(R.id.album_list);
-//        albumList.setAdapter(new AlbumAdapter(this));
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
         List<Fragment> fragmentList = new ArrayList<>();
         fragmentList.add(new FragmentUser());
@@ -43,5 +44,30 @@ public class AlbumActivity extends AppCompatActivity {
     protected void onSaveInstanceState(Bundle outState) {
         mTabLayout.saveState(outState);
         super.onSaveInstanceState(outState);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        //return super.onOptionsItemSelected(item);
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                // User chose the "Settings" item, show the app settings UI...
+                Toast.makeText(getApplicationContext(), "환경설정 버튼 클릭됨", Toast.LENGTH_LONG).show();
+                return true;
+
+            default:
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                Toast.makeText(getApplicationContext(), "나머지 버튼 클릭됨", Toast.LENGTH_LONG).show();
+                return super.onOptionsItemSelected(item);
+
+        }
     }
 }
