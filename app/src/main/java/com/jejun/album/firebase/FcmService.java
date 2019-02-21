d001b9bdf36be4004026a98c573a012d9d95a4d7
jejun <jejun.jade@gmail.com>
2019-02-21 23:36:13 +0900

    *firebase build project

---
+++ app/src/main/java/com/jejun/album/firebase/FcmService.java
@@ -0,0 +1,12 @@
+package com.jejun.album.firebase;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+public class FcmService extends FirebaseMessagingService {
+
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+//        handleRemoteMessage(remoteMessage, this);
+    }
+}
