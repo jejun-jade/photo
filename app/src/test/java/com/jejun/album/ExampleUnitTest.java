7df34e82588be6df31df04f7af2966262b452e01
jejun <jejun@jade@gmail.com>
2019-03-07 23:18:11 +0900

    * add test code sample

--- app/src/test/java/com/jejun/album/ExampleUnitTest.java
+++ app/src/test/java/com/jejun/album/ExampleUnitTest.java
@@ -1,6 +1,11 @@
 package com.jejun.album;
 
+import com.jejun.album.object.Album;
+import com.jejun.album.user.LoginActivity;
+
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
 import static org.junit.Assert.*;
 
@@ -13,5 +18,16 @@ public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() {
         assertEquals(4, 2 + 2);
+    }
+
+    @RunWith(AndroidJUnit4.class)
+    @LargeTest
+    public class MyClassTest {
+
+        @Rule
+        public ActivityTestRule<Album> mActivityRule = new ActivityTestRule(LoginActivity.class);
+
+        @Test
+        public void myClassMethod_ReturnsTrue() { }
     }
 }
